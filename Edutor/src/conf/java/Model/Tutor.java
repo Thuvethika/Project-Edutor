1cf7056dece6900fb43539d21ceb424a6070061e
Thuvethika <62344516+Thuvethika@users.noreply.github.com>
2021-10-13 17:11:37 +0530

    Update Tutor.java

--- Edutor/src/conf/java/Model/Tutor.java
+++ Edutor/src/conf/java/Model/Tutor.java
@@ -5,10 +5,140 @@
  */
 package Model;
 
+import java.sql.Blob;
+
 /**
  *
  * @author thuve
  */
 public class Tutor {
-    
+
+    private String firstname;
+    private String lastname;
+    private String address;
+    private String gender;
+    private String dob;
+    private int phoneno;
+    private String degreename;
+    private String qualification;
+    private String email;
+    private String password;
+    private String nic;
+    private Blob img;
+
+    public Tutor() {
+
+    }
+
+    public Tutor(String firstname, String lastname, String address, String gender, String dob, String nic, int phoneno, String degree, String email, String password, String qualification, Blob img) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.address = address;
+        this.gender = gender;
+        this.dob = dob;
+        this.nic = nic;
+        this.phoneno = phoneno;
+        this.degreename = degree;
+        this.email = email;
+        this.password = password;
+        this.qualification = qualification;
+        this.img = img;
+
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public int getPhoneno() {
+        return phoneno;
+    }
+
+    public void setPhoneno(int phoneno) {
+        this.phoneno = phoneno;
+    }
+
+    public String getDegreename() {
+        return degreename;
+    }
+
+    public void setDegreename(String degreename) {
+        this.degreename = degreename;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNic() {
+        return nic;
+    }
+
+    public void setNic(String nic) {
+        this.nic = nic;
+    }
+
+    public String getQualification() {
+        return qualification;
+    }
+
+    public void setQualification(String qualification) {
+        this.qualification = qualification;
+    }
+
+    public Blob getImg() {
+        return img;
+    }
+
+    public void setImg(Blob img) {
+        this.img = img;
+    }
 }
