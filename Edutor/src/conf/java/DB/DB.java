4aa5c945c5e8c112ee2353a70867d31965c57f49
Thuvethika <62344516+Thuvethika@users.noreply.github.com>
2021-10-13 17:09:28 +0530

    Update DB.java

--- Edutor/src/conf/java/DB/DB.java
+++ Edutor/src/conf/java/DB/DB.java
@@ -5,10 +5,38 @@
  */
 package DB;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author thuve
  */
 public class DB {
-    
+     private static Connection con;
+
+    private static synchronized Connection getConnection() {
+
+        if (con == null) {
+
+            try {
+
+                Class.forName("com.mysql.jdbc.Driver");
+                con = DriverManager.getConnection("jdbc:mysql://localhost:3306/edutor", "root", "");
+            } catch (ClassNotFoundException | SQLException ex) {
+                Logger.getLogger(DB.class.getName()).log(Level.SEVERE, null, ex);
+
+            }
+        }
+        return con;
+    }
+
+    public static void iud(String query) throws SQLException {
+        getConnection();
+        getConnection().createStatement().executeUpdate(query);
+
+    }
 }
