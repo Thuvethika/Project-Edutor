eb26f2d70e8a6d0944f02bfb9a8055ed345d2e4a
Thineshkanth <84115197+Thineshkanth@users.noreply.github.com>
2021-10-17 21:19:12 +0530

    Delete Edutor/web/View directory

--- Edutor/web/View/Signup/side-nav.js
+++
@@ -1,8 +0,0 @@
-/*sidepannel*/
-function openNav() {
-  document.getElementById("mySidenav").style.display = "block";
-}
-
-function closeNav() {
-  document.getElementById("mySidenav").style.display = "none";
-}
