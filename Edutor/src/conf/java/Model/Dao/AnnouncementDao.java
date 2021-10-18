be7d1e94afd6040588cdc41c565fa3bd43072b0f
Thuvethika <62344516+Thuvethika@users.noreply.github.com>
2021-10-18 16:00:02 +0530

    AnnouncementDao file

---
+++ Edutor/src/conf/java/Model/Dao/AnnouncementDao.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model.Dao;
+
+import Model.Announcement;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author thuve
+ */
+public interface AnnouncementDao {
+    public void addAnnouncement(Announcement announcement, Announcement announcement1) throws SQLException ;
+    
+   
+}
