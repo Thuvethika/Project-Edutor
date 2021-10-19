4b8712e676e8ced839fa8fac567e19aabdd9042e
Thuvethika <62344516+Thuvethika@users.noreply.github.com>
2021-10-19 13:53:16 +0530

    Model - Admin delete user

---
+++ Edutor/src/conf/java/Model/DeleteUser.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author thuve
+ */
+public class DeleteUser {
+    private String email;
+    
+    public DeleteUser(){
+    
+    }
+    
+    public DeleteUser(String email){
+    this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+}
