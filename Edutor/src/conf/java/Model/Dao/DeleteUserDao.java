55a5df473a04c8614bdc22956b840d3e42856d0e
Thuvethika <62344516+Thuvethika@users.noreply.github.com>
2021-10-19 13:54:31 +0530

    Deleteuser Dao file

---
+++ Edutor/src/conf/java/Model/Dao/DeleteUserDao.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model.Dao;
+
+import Model.DeleteUser;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author thuve
+ */
+public interface DeleteUserDao {
+    public String Deleteuser(DeleteUser deleteuser) throws SQLException;
+}
