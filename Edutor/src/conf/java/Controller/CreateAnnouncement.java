e167587ca87fb38629672262710eb9ff990623fc
Thuvethika <62344516+Thuvethika@users.noreply.github.com>
2022-01-05 17:40:03 +0530

    Add files via upload

--- Edutor/src/conf/java/Controller/CreateAnnouncement.java
+++ Edutor/src/conf/java/Controller/CreateAnnouncement.java
@@ -31,29 +31,36 @@ public class CreateAnnouncement extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
       //  processRequest(request, response);
-
+//  int classid1 = Integer.parseInt(request.getParameter("classid"));
         String topic = request.getParameter("topic");
         String date = request.getParameter("date");
-        String link = request.getParameter("link");
+     
         String details = request.getParameter("details");
-
-        try (PrintWriter out = response.getWriter()) {
+PrintWriter out = response.getWriter();
+        try {
             
             AnnouncementDao announcement = new AnnouncementDaoImpl();
             
             Announcement A1 = new Announcement();
             A1.setTopic(topic);
             A1.setDate(date);
-            A1.setLink(link);
+         
             A1.setDetails(details);
-            
-             HttpSession session = request.getSession(false);
-                String email = (String) session.getAttribute("email");
-                Announcement A2 = new Announcement();
-                A2.setEmail(email);
+            HttpSession session = request.getSession(false);
+            String email = (String) session.getAttribute("email");
+            A1.setEmail(email);
+             HttpSession session1 = request.getSession(false);
+           
+     int classid = ((Integer) session1.getAttribute("classid"));
+     A1.setClassid(classid);
+                //Announcement A2 = new Announcement();
+              //  A2.setEmail(email);
                
-            announcement.addAnnouncement(A1,A2);
-            out.println("insertedd");
+            announcement.addAnnouncement(A1);
+         //   out.println("insertedd");
+            String site=new String("view/Announce/DisplayAnnouncement.jsp");
+                response.setStatus(response.SC_MOVED_TEMPORARILY);
+                response.setHeader("location", site);
 
         } catch (SQLException ex) {
             Logger.getLogger(CreateAnnouncement.class.getName()).log(Level.SEVERE, null, ex);
