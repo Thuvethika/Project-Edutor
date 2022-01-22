b949fb9c84acf1e4a47e4a2fea77407afa9ae05e
Thishan-Paramanayagam <84135246+Thishan-Paramanayagam@users.noreply.github.com>
2022-01-22 19:07:11 +0530

    Add files via upload

---
+++ Edutor/web/view/Script/script.js
@@ -0,0 +1,19 @@
+function displaySignup() {
+    console.log("Hello world");
+  
+    var popWindow = document.querySelector(".signup-form");
+    var overlay = document.querySelector(".overlay");
+  
+    popWindow.classList.toggle("hidden");
+    overlay.classList.toggle("hidden");
+  }
+  
+  document.getElementById("popWindow").addEventListener("click", function () {
+    displaySignup();
+  });
+
+
+
+
+  
+
